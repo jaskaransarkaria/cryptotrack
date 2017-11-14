0d409d5f0cee9f3d60fe2fc3d1f6f2fb49ae1f47
jaskaran <jaskaran.sarkaria@googlemail.com>
2017-11-14 16:05:39 +0000

    print anchor, time stamp, hyperlink, limit=10

--- news_tweets.py
+++ news_tweets.py
@@ -2,31 +2,22 @@
 # google news
 
 import urllib
-from urllib.request import urlopen, Request
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
 def make_soup(url): #in order to parse the webpage for info
     the_page = urllib.request.urlopen(url)
     soup_data = BeautifulSoup(the_page, 'html.parser')
     return soup_data
 
-def click_link(old_link, new_link): #this is for opening headlines... I don't need it atm
-
-    driver = webdriver.chrome()
-    driver.get(old_link)
-    elem1 = driver.find_element_by_link_text(new_link) #these will be the headlines
-    elem1.click()
-    current_url = driver.current_url
-    driver.implicitly_wait(60)  # 60 seconds
-    soup3 = make_soup(current_url)
-    soup5 = soup3.get_text()
-
-
-
 soup = make_soup('https://news.google.com/news/search/section/q/bitcoin')
-for anchor in soup.find_all('a'):
-    #elem2 = driver.find_element_by_partial_link_text() #need to find just bitcoin info
-    print(anchor.text) # prints ALL anchor tags at the minute I just need the first relevant 5
-    html_link = anchor.get("href")
-    print(html_link)
+bitcoin = soup.find_all("a", class_="nuEeue hzdq5d ME7ew", limit=10)
+time_ago = soup.find_all("span", class_="d5kXP YBZVLb", limit=10)
+
+for v in bitcoin:
+    anchor = v.string.strip()
+    hyperlink = v.get("href")
+    for i in time_ago:
+        time_stamp = i.string.strip()
+    print(anchor + " ." + time_stamp + " ")
+    print(hyperlink)
