bfaf31db03a908c5791f16fe90f376869d7c9599
jaskaran <jaskaran.sarkaria@googlemail.com>
2017-11-21 10:45:23 +0000

    add ethereum and iota

--- news_tweets.py
+++ news_tweets.py
@@ -10,15 +10,27 @@ def make_soup(url): #in order to parse the webpage for info
     soup_data = BeautifulSoup(the_page, 'html.parser')
     return soup_data
 
-soup = make_soup('https://news.google.com/news/search/section/q/bitcoin')
-bitcoin = soup.find_all("a", class_="nuEeue hzdq5d ME7ew", limit=10)
-time_ago = soup.find_all("span", class_="d5kXP YBZVLb", limit=10)
+def coin_info(coin):
+    coinsoup = make_soup('https://news.google.com/news/search/section/q/' + coin)
+    coin = coinsoup.find_all("a", class_="nuEeue hzdq5d ME7ew", limit=10)
+    time_ago = coinsoup.find_all("span", class_="d5kXP YBZVLb", limit=10)
 
-for v, i in zip(bitcoin, time_ago):
-    anchor = v.string.strip()
-    hyperlink = v.get("href")
-    time_stamp = i.string.strip()
-    print(anchor + ". " + time_stamp + ".")
-    print(hyperlink)
+    for v, i in zip(coin, time_ago):
+        anchor = v.string.strip()
+        hyperlink = v.get("href")
+        time_stamp = i.string.strip()
+        print(anchor + ". " + time_stamp + ".")
+        print(hyperlink)
 
+
+
+coin_info("bitcoin")
+print("\n")
+print("\n")
+print("\n")
+coin_info("ethereum")
+print("\n")
+print("\n")
+print("\n")
+coin_info("iota")
 #need to add timed running code
