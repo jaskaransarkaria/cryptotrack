99bc2f0e59e59f2b89a89fdb4eca4d82f3280a60
jaskaran <jaskaran.sarkaria@googlemail.com>
2017-11-28 13:39:59 +0000

    add event scheduler

--- crypto.py
+++ crypto.py
@@ -1,7 +1,7 @@
 '''track cryptocurrencies on the stock markets'''
 # coinmarketcap.com
 import coinmarketcap
-import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
 import pandas as pd
 import urllib
 from urllib.request import urlopen
@@ -14,19 +14,19 @@ def make_soup(url): #in order to parse the webpage for info
     soup_data = BeautifulSoup(the_page, 'html.parser')
     return soup_data
 
-def coin_info(coin):
+def coin_info(coin): # parse the top ten news for each coin
     coinsoup = make_soup('https://news.google.com/news/search/section/q/' + coin)
     coin = coinsoup.find_all("a", class_="nuEeue hzdq5d ME7ew", limit=10)
     time_ago = coinsoup.find_all("span", class_="d5kXP YBZVLb", limit=10)
 
-    for v, i in zip(coin, time_ago):
+    for v, i in zip(coin, time_ago): # print coin and time tag simultaneously
         anchor = v.string.strip()
         hyperlink = v.get("href")
         time_stamp = i.string.strip()
         print(anchor + ". " + time_stamp + ".")
         print(hyperlink)
 
-def coin_price(coin):
+def coin_price(coin): # connect to api and print infor
     market = coinmarketcap.Market()
     currency = market.ticker(coin)
     price_table = pd.Series((currency)[0]) #need to format and cut out unneeded data from this pandas
@@ -35,7 +35,7 @@ def coin_price(coin):
     #need to only collect "name", "rank", "price_usd", "percent_change_1hr", "percent_change_24hr"
     # "percent_change_7d"
 
-while True:
+def coin_and_news():
     coin_price("bitcoin")
     coin_info("bitcoin")
     print("\n")
@@ -48,4 +48,7 @@ while True:
     print("\n")
     coin_price("iota")
     coin_info("iota")
-    sleep(3600) # wait 1 hour
+
+sched = BlockingScheduler()
+sched.add_job(coin_and_news, 'cron', hour='10-22', minute='0,30') # scheduled to run between the hours 10 and 10pm
+sched.start()
